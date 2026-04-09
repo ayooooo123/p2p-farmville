1f87c17797adf629f01fbe5508ef66f9c06e52a1
ayooooo123 <248791769+ayooooo123@users.noreply.github.com>
2026-04-09 00:01:43 -0400

    Use PearRuntime upgrade option instead of link

--- src/bun/index.ts
+++ src/bun/index.ts
@@ -64,7 +64,7 @@ async function startWorker(storagePath: string) {
     dir: storagePath,
     version: APP_VERSION,
     name: APP_NAME,
-    link: 'pear://dev',
+    upgrade: 'pear://dev',
     updates: false,
   });
 
